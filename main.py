4dc14f2aa870f1041d050410ca104537a959b516
Certwilo <131922@secondary.isalmere.nl>
2026-04-13 10:39:44 +0200

    yes

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ def tournament_config():
     lastname_box.insert(0, "Enter last name")
     lastname_box.pack(pady=10) 
 
+
     tk.Label(screen, text="Student Number", font=("Times New Roman", 15)).pack(pady=20)
     studentnumber_box = tk.Entry(screen, bg="white", fg="black")
     studentnumber_box.insert(0, "Enter your student number")
@@ -33,7 +34,7 @@ def tournament_config():
     password.insert(0, "At least 8 characters long")
     password.pack(pady=10)  
 
-    next_button = tk.Button(screen, text="Next", command=None)
+    next_button = tk.Button(screen, text="Next", command=individual_or_team)
     next_button.pack(pady=10) 
 
 def login_screen(): 
@@ -45,14 +46,29 @@ def login_screen():
     studentnumber_box = tk.Entry(screen, bg="white", fg="black")
     studentnumber_box.insert(0, "Enter your student number")
     studentnumber_box.pack(pady=10)
-    
+
     tk.Label(screen, text="Password", font=("Times New Roman", 15)).pack(pady=20)
     password = tk.Entry(screen, bg="white", fg="black")
     password.insert(0, "At least 8 characters long")
     password.pack(pady=10)
 
-    next_button = tk.Button(screen, text="Next", command=None)
-    next_button.pack(pady=10)
+    next_button = tk.Button(screen, text="Next", command=individual_or_team)
+    next_button.pack(pady=10)  
+
+def individual_or_team(): 
+    clear_window()
+    screen.title("Hello", student_name)
+    tk.Label(screen,font=("Times New Roman", 35)).pack(pady=20)
+
+    individual_button = tk.Button(screen, text="individual", command=events) 
+    individual_button.pack(pady=10)
+
+    team_button = tk.Button(screen, text="team", command=None)
+    team_button.pack(pady=10)
+
+def events(): 
+    "jfio"
+
 
 screen = tk.Tk()
 screen.geometry("800x600")
@@ -67,13 +83,22 @@ button_frame.pack(pady=10)
 login_button = tk.Button(button_frame, text="Log in", command=login_screen)
 login_button.pack(padx=10,pady=10)   
 
+firstname_box = "" 
+secondname_box =  ""
+
+student_name = firstname_box + secondname_box 
+
 signup_button = tk.Button(button_frame, text="Sign up", command=tournament_config)
-signup_button.pack(side="right",padx=10,pady=10)
+signup_button.pack(side="right",padx=10,pady=10) 
 
+next_button = tk.Button(screen, text="Next", command=individual_or_team) 
+next_button.pack(pady=10)
 
+individual_button = tk.Button(screen, text="individual", command=events) 
+individual_button.pack(pady=10)
 
-
-
+team_button = tk.Button(screen,text="team", command=events)
+team_button.pack(side="right",padx=10,pady=10)
 
 
 screen.mainloop() 
